3357ac20fc15e7ac48e751e3f11c026728fc8ec4
Rich Neswold <neswold@fnal.gov>
2020-03-18 11:20:35 -0500

    Add `try-catch` to catch uncaught exceptions.

--- bin/acnet_main.dart
+++ bin/acnet_main.dart
@@ -3,28 +3,33 @@ import '../lib/level2.dart';
 
 void main() async {
   final c = Connection();
-  final h = await c.handle;
-  final ln = await c.getLocalNode();
+  try {
+    final h = await c.handle;
+    final ln = await c.getLocalNode();
 
-  print("Handle '$h' on local node '$ln'");
+    print("Handle '$h' on local node '$ln'");
 
-  final tn = await c.getNodeAddress("CLX73");
-  final nm = await c.getNodeName(tn);
+    final tn = await c.getNodeAddress("CLX73");
+    final nm = await c.getNodeName(tn);
 
-  print("CLX73: tn = $tn, nm = $nm");
+    print("CLX73: tn = $tn, nm = $nm");
 
-  final node = "CLX39";
-  final answering = await c.ping(node: node);
+    final node = "CLX39";
+    final answering = await c.ping(node: node);
 
-  if (answering) {
-    final v = await c.version(node: node);
+    if (answering) {
+      final v = await c.version(node: node);
 
-    print("${v[0]}, ${v[1]}, ${v[2]}");
-  } else
-    print("$node did not answer.");
+      print("${v[0]}, ${v[1]}, ${v[2]}");
+    } else
+      print("$node did not answer.");
 
-  final tasks = await c.getTasks(node: node);
+    final tasks = await c.getTasks(node: node);
 
-  for (var ii in tasks)
-    print(ii.toString());
+    for (var ii in tasks)
+      print(ii.toString());
+  }
+  catch (e) {
+    print("Caught exception: $e");
+  }
 }
