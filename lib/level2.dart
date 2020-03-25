326245a7978c7100184bdd5b7ef260bd38fdc0d1
Rich Neswold <neswold@fnal.gov>
2020-03-25 14:10:32 -0500

    Alphabetize the methods.

--- lib/level2.dart
+++ lib/level2.dart
@@ -36,44 +36,6 @@ class TaskInfo {
 /// ```
 
 extension Level2 on Connection {
-  /// Pings the specified ACNET node. If this method returns [true], the remote
-  /// node responded.
-  Future<bool> ping({String node}) async {
-    final result = await this.requestReply(
-        task: "ACNET@" + node,
-        data: Uint8List.fromList(const [0, 0]),
-        timeout: 100);
-
-    return result.status.isGood && result.message.length == 2;
-  }
-
-  /// Get the versions associated with the specified ACNET node.
-  ///
-  /// The return value is a list of 3 strings representing the three version
-  /// numbers used to identify aspect of ACNET. The first version represents
-  /// the version of the network layout. ACNET nodes with the same, first
-  /// version should be able to communicate. The second version is associated
-  /// with internals of the local ACNET process/library. The third version
-  /// represent the local API that clients use to communicate with their ACNET
-  /// process/library.
-  Future<List<String>> getVersions({String node}) async {
-    final result = await this.requestReply(
-        task: "ACNET@" + node,
-        data: Uint8List.fromList(const [3, 0]),
-        timeout: 100);
-
-    if (result.status.isGood) {
-      final v = Uint8List.fromList(result.message);
-      final bd = ByteData.view(v.buffer);
-
-      return [
-        bd.getUint16(0, Endian.little),
-        bd.getUint16(2, Endian.little),
-        bd.getUint16(4, Endian.little)
-      ].map((v) => "${v ~/ 256}.${v % 256}").toList();
-    } else
-      throw result.status;
-  }
 
   /// Retrieves a snapshot of the tasks connected to an ACNET node.
   Future<Map<int, TaskInfo>> getTasks({String node}) async {
@@ -104,5 +66,44 @@ extension Level2 on Connection {
         throw ACNET_TRUNC_REPLY;
     } else
       throw result.status;
+  }
+
+  /// Get the versions associated with the specified ACNET node.
+  ///
+  /// The return value is a list of 3 strings representing the three version
+  /// numbers used to identify aspect of ACNET. The first version represents
+  /// the version of the network layout. ACNET nodes with the same, first
+  /// version should be able to communicate. The second version is associated
+  /// with internals of the local ACNET process/library. The third version
+  /// represent the local API that clients use to communicate with their ACNET
+  /// process/library.
+  Future<List<String>> getVersions({String node}) async {
+    final result = await this.requestReply(
+        task: "ACNET@" + node,
+        data: Uint8List.fromList(const [3, 0]),
+        timeout: 100);
+
+    if (result.status.isGood) {
+      final v = Uint8List.fromList(result.message);
+      final bd = ByteData.view(v.buffer);
+
+      return [
+        bd.getUint16(0, Endian.little),
+        bd.getUint16(2, Endian.little),
+        bd.getUint16(4, Endian.little)
+      ].map((v) => "${v ~/ 256}.${v % 256}").toList();
+    } else
+      throw result.status;
+  }
+
+  /// Pings the specified ACNET node. If this method returns [true], the remote
+  /// node responded.
+  Future<bool> ping({String node}) async {
+    final result = await this.requestReply(
+        task: "ACNET@" + node,
+        data: Uint8List.fromList(const [0, 0]),
+        timeout: 100);
+
+    return result.status.isGood && result.message.length == 2;
   }
 }
